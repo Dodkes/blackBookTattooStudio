9ee4b28fefd466b1e6bf51b6d556d26688ee97fc
Jozef_Zitt <dodo.zitt@gmail.com>
2024-11-08 21:25:15 +0100

    akar icons & navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,34 @@
+import { InstagramFill, Envelope, FacebookFill, Phone } from "akar-icons";
+import logo from "../assets/logo.jpg";
+
 export default function Navbar() {
-  return <nav className="navbar">Navbar</nav>;
+  return (
+    <nav className="navbar">
+      <ul className="navbar-items">
+        <li>
+          <a href="">
+            <InstagramFill size={15} className="navbar-item-icon" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <Envelope size={15} className="navbar-item-icon" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <FacebookFill size={15} className="navbar-item-icon" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <Phone size={15} className="navbar-item-icon" />
+          </a>
+        </li>
+      </ul>
+      <a className="logo-link" href="">
+        <img src={logo} width={200} />
+      </a>
+    </nav>
+  );
 }
