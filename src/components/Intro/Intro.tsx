8c66a86dbdb7b9a015c849b89fb4d2fbcadc9410
Jozef_Zitt <dodo.zitt@gmail.com>
2024-11-08 21:50:37 +0100

    add assets

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,3 +1,10 @@
+import profile from "../../assets/profile.jpg";
+// import profile from "../../assets/profile2.jpg";
+
 export default function Intro() {
-  return <div className="intro-container"></div>;
+  return (
+    <div className="intro-container">
+      <img src={profile} alt="profile" width={900} />
+    </div>
+  );
 }
