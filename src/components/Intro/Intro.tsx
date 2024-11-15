bf06198a3d4624bea4e755595288fcbe0cc65431
Jozef_Zitt <dodo.zitt@gmail.com>
2024-11-15 21:15:36 +0100

    add intro video

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,7 +1,12 @@
+import video from "../../assets/video.mp4";
+
 export default function Intro() {
   return (
     <div className="intro-container">
       <h1 className="intro-heading">Profesionální tetovací studio v Čechách</h1>
+      <video className="intro-video" autoPlay loop muted>
+        <source src={video} type="video/mp4" />
+      </video>
     </div>
   );
 }
