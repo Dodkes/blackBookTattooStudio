7f12de669dc7e7dee1cba05e6112592ea15bc419
Jozef_Zitt <dodo.zitt@gmail.com>
2024-11-15 21:19:26 +0100

    "playsInline" attribute

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -4,7 +4,7 @@ export default function Intro() {
   return (
     <div className="intro-container">
       <h1 className="intro-heading">Profesionální tetovací studio v Čechách</h1>
-      <video className="intro-video" autoPlay loop muted>
+      <video className="intro-video" autoPlay loop muted playsInline>
         <source src={video} type="video/mp4" />
       </video>
     </div>
