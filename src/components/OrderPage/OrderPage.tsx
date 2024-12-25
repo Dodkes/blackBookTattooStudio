96804ad2dba4fd566ee2df6e5f7d45ecebafe061
Jozef_Zitt <dodo.zitt@gmail.com>
2024-12-25 12:07:49 +0100

    feat(FE): add form + validation + error handling

--- src/components/OrderPage/OrderPage.tsx
+++ src/components/OrderPage/OrderPage.tsx
@@ -1,8 +1,161 @@
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+type FormValues = {
+  name: string;
+  surname: string;
+  email: string;
+  phone: string;
+  message: string;
+  service: string;
+  color: string;
+};
 
 function OrderBody() {
-  return <div className="order-body-container">Test</div>;
+  const formInitialValues = {
+    name: "",
+    surname: "",
+    email: "",
+    phone: "",
+    message: "",
+    service: "tattoo",
+    color: "black",
+  };
+
+  const formValidationSchema = Yup.object().shape({
+    name: Yup.string().required("Jméno je povinné"),
+    surname: Yup.string().required("Příjmení je povinné"),
+    email: Yup.string()
+      .email("Email není ve správném formátu")
+      .required("Email je povinný"),
+    phone: Yup.string()
+      .required("Telefon je povinný")
+      .min(7, "Telefon je krátký")
+      .max(15, "Telefon je dlouhý")
+      .matches(
+        /^[+]?[0-9]*$/,
+        "Telefon může obsahovat pouze číslice a znak '+'"
+      ),
+  });
+
+  const handleSubmit = (values: FormValues) => {
+    try {
+      console.log(values);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="order-body-container">
+      <h1 className="order-heading">Objednat si termín</h1>
+      <Formik
+        initialValues={formInitialValues}
+        validationSchema={formValidationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({
+          values,
+          handleChange,
+          handleSubmit,
+          isSubmitting,
+          isValid,
+          errors,
+        }) => (
+          <form className="order-form" onSubmit={handleSubmit}>
+            <div className="order-form-field">
+              <label htmlFor="name">Jméno *</label>
+              <br />
+              <input
+                type="text"
+                name="name"
+                value={values.name}
+                onChange={handleChange}
+              />
+              <p className="form-field-error-message">{errors.name}</p>
+            </div>
+            <div className="order-form-field">
+              <label htmlFor="surname">Příjmení *</label>
+              <br />
+              <input
+                type="text"
+                name="surname"
+                value={values.surname}
+                onChange={handleChange}
+              />
+              <p className="form-field-error-message">{errors.surname}</p>
+            </div>
+            <div className="order-form-field">
+              <label htmlFor="email">Email *</label>
+              <br />
+              <input
+                type="email"
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                placeholder="@"
+              />
+              <p className="form-field-error-message">{errors.email}</p>
+            </div>
+            <div className="order-form-field">
+              <label htmlFor="phone">Telefon *</label>
+              <br />
+              <input
+                type="tel"
+                name="phone"
+                value={values.phone}
+                onChange={handleChange}
+              />
+              <p className="form-field-error-message">{errors.phone}</p>
+            </div>
+            <div className="order-form-field">
+              <label htmlFor="service">Výběr služby</label>
+              <br />
+              <select
+                name="service"
+                defaultValue={values.service}
+                onChange={handleChange}
+              >
+                <option value="tattoo">Tetování</option>
+                <option value="barber">Barber</option>
+              </select>
+            </div>
+            <div className="order-form-field">
+              <label htmlFor="color">Barva</label>
+              <br />
+              <select
+                name="color"
+                defaultValue={values.color}
+                onChange={handleChange}
+              >
+                <option value="black">Černá</option>
+                <option value="colorful">Barevná</option>
+              </select>
+            </div>
+            <div className="order-form-field message">
+              <label htmlFor="message">Popis tetování</label>
+              <textarea
+                maxLength={1000}
+                name="message"
+                value={values.message}
+                onChange={handleChange}
+                placeholder="Zpráva"
+              />
+            </div>
+            <button
+              disabled={isSubmitting || !isValid}
+              className="button-order-submit"
+              type="submit"
+            >
+              Odeslat
+            </button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
 export default function OrderPage() {
