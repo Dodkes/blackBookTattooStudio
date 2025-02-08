500ee4d354c175df8d7bca89baed5a10178d7918
Jozef_Zitt <dodo.zitt@gmail.com>
2025-02-08 09:48:25 +0100

    testing correct files

--- scripts/deploy-front-end.sh
+++ scripts/deploy-front-end.sh
@@ -8,4 +8,5 @@ chmod 600 ~/.ssh/id_rsa
 ssh-keyscan 188.245.201.83 >> ~/.ssh/known_hosts #ass server to known hosts - so it does not ask for confirmation when connecting to the server for the first time
 echo "PWD:"
 pwd
+ls
 # scp -r ./dist/. root@188.245.201.83:/var/www/html/.
