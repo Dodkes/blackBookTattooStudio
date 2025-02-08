eed992403f14c3c07db1238ab33a67c8d7b9bec1
Jozef_Zitt <dodo.zitt@gmail.com>
2025-02-08 09:43:17 +0100

    ci/cd path try

--- scripts/deploy-front-end.sh
+++ scripts/deploy-front-end.sh
@@ -6,4 +6,5 @@ mkdir -p ~/.ssh
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keyscan 188.245.201.83 >> ~/.ssh/known_hosts #ass server to known hosts - so it does not ask for confirmation when connecting to the server for the first time
-scp -r ./blackbooktattoostudio/dist/. root@188.245.201.83:/var/www/html/.
+pwd
+scp -r ./dist/. root@188.245.201.83:/var/www/html/.
