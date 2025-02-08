06b2e1af5b3e469ffba629df60d74fcd6bf3e9cd
Jozef_Zitt <dodo.zitt@gmail.com>
2025-02-08 09:41:39 +0100

    cdi/cd: fix local path to build version

--- scripts/deploy-front-end.sh
+++ scripts/deploy-front-end.sh
@@ -6,4 +6,4 @@ mkdir -p ~/.ssh
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keyscan 188.245.201.83 >> ~/.ssh/known_hosts #ass server to known hosts - so it does not ask for confirmation when connecting to the server for the first time
-scp -r /dist/. root@188.245.201.83:/var/www/html/.
+scp -r ./blackbooktattoostudio/dist/. root@188.245.201.83:/var/www/html/.
