40c318d2d30bcdbae96a64e5f7a250ee9a49d10c
Jozef_Zitt <dodo.zitt@gmail.com>
2025-02-09 19:54:03 +0100

    remove port 2222 from all scripts

--- scripts/show-loggin-attempts.sh
+++ scripts/show-loggin-attempts.sh
@@ -5,4 +5,4 @@
 # Hackers commonly use brute force attacks to gain access to servers with common usernames like root/administrator/ubuntu/debian etc.
 # This script will show the last 500 lines of the log file to show the login attempts on the server.
 
-ssh -p 2222 root@188.245.201.83 "sudo tail -f /var/log/auth.log"
+ssh root@188.245.201.83 "sudo tail -f /var/log/auth.log"
--- scripts/show-logs.sh
+++ scripts/show-logs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -p 2222 root@188.245.201.83 "cd /var/www/server && cd /var/www/server/logs && tail -500 app.log"
+ssh root@188.245.201.83 "cd /var/www/server && cd /var/www/server/logs && tail -500 app.log"
