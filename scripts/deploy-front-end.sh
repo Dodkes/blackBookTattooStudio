d4226eceb1acc74a1a744220310f86402fa07fe4
Jozef_Zitt <dodo.zitt@gmail.com>
2025-02-09 11:21:18 +0100

    scp -P

--- scripts/deploy-front-end.sh
+++ scripts/deploy-front-end.sh
@@ -6,4 +6,4 @@ mkdir -p ~/.ssh
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -p 2222 188.245.201.83 >> ~/.ssh/known_hosts #ass server to known hosts - so it does not ask for confirmation when connecting to the server for the first time
-scp -p 2222 -r ./dist/. root@188.245.201.83:/var/www/html/.
+scp -P 2222 -r ./dist/. root@188.245.201.83:/var/www/html/.
