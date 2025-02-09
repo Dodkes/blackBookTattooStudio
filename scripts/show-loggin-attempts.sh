fb09a02e696caa762d89b11ae24fde94dd5c5360
Jozef_Zitt <dodo.zitt@gmail.com>
2025-02-09 18:34:19 +0100

    -f follow auth logs

--- scripts/show-loggin-attempts.sh
+++ scripts/show-loggin-attempts.sh
@@ -5,7 +5,4 @@
 # Hackers commonly use brute force attacks to gain access to servers with common usernames like root/administrator/ubuntu/debian etc.
 # This script will show the last 500 lines of the log file to show the login attempts on the server.
 
-ssh -p 2222 root@188.245.201.83 "sudo grep "Failed password" /var/log/auth.log | tail -500"
-
-# If needed, block the IP address (FW) of the attacker with the following command:
-# sudo ufw deny from 80.242.208.68
+ssh -p 2222 root@188.245.201.83 "sudo tail -f /var/log/auth.log"
