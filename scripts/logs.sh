cfe38949d6ecf2dff0ff6790600d3de07f52ee6b
Jozef_Zitt <dodo.zitt@gmail.com>
2025-03-14 15:12:33 +0100

    optimise scripts

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -3,4 +3,4 @@
 LINES=${1:-500}
 
 #show last n lines of app.log | default 500 lines
-ssh root@188.245.201.83 "cd /var/www/server && cd /var/www/server/logs && tail -n $LINES app.log"
+ssh root@188.245.201.83 "cd /var/www/server/logs && tail -n $LINES app.log"
--- scripts/status.sh
+++ scripts/status.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo STATUS:
-ssh root@188.245.201.83 "uptime"
-echo FILE SYSTEM:
-ssh root@188.245.201.83 "df -h"
+ssh root@188.245.201.83 "uptime && df -h"
